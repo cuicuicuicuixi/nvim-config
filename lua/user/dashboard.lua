179e46f018ffaf38cebed608a925c89b201266fd
cuicuicuicuixi <849343517@qq.com>
2022-07-13 17:31:17 +0800

    init

---
+++ lua/user/dashboard.lua
@@ -0,0 +1,63 @@
+local status_ok, db = pcall(require, "dashboard")
+if not status_ok then
+    return
+end
+
+local home = os.getenv('HOME')
+db.custom_center = {
+      {icon = '  ',
+      desc = 'Recently latest session                  ',
+      shortcut = 'SPC s l',
+      action ='SessionLoad'},
+      {icon = '  ',
+      desc = 'Recently opened files                   ',
+      action =  'DashboardFindHistory',
+      shortcut = 'SPC f h'},
+      {icon = '  ',
+      desc = 'Find  File                              ',
+      action = 'Telescope find_files find_command=rg,--hidden,--files',
+      shortcut = 'SPC f f'},
+      {icon = '  ',
+      desc ='File Browser                            ',
+      action =  'Telescope file_browser',
+      shortcut = 'SPC f b'},
+      {icon = '  ',
+      desc = 'Find  word                              ',
+      action = 'Telescope live_grep',
+      shortcut = 'SPC f w'},
+      {icon = '  ',
+      desc = 'Open Personal dotfiles                  ',
+      action = 'Telescope dotfiles path=' .. home ..'/.dotfiles',
+      shortcut = 'SPC f d'},
+}
+
+db.default_executive = 'telescope'
+
+-- db.custom_header= {
+--     [[    ⢰⣧⣼⣯⠄⣸⣠⣶⣶⣦⣾⠄⠄⠄⠄⡀⠄⢀⣿⣿⠄⠄⠄⢸⡇⠄⠄ ]],
+--     [[    ⣾⣿⠿⠿⠶⠿⢿⣿⣿⣿⣿⣦⣤⣄⢀⡅⢠⣾⣛⡉⠄⠄⠄⠸⢀⣿⠄ ]],
+--     [[   ⢀⡋⣡⣴⣶⣶⡀⠄⠄⠙⢿⣿⣿⣿⣿⣿⣴⣿⣿⣿⢃⣤⣄⣀⣥⣿⣿⠄ ]],
+--     [[   ⢸⣇⠻⣿⣿⣿⣧⣀⢀⣠⡌⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⣿⣿⣿⠄ ]],
+--     [[  ⢀⢸⣿⣷⣤⣤⣤⣬⣙⣛⢿⣿⣿⣿⣿⣿⣿⡿⣿⣿⡍⠄⠄⢀⣤⣄⠉⠋⣰ ]],
+--     [[  ⣼⣖⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⢇⣿⣿⡷⠶⠶⢿⣿⣿⠇⢀⣤ ]],
+--     [[ ⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣷⣶⣥⣴⣿⡗ ]],
+--     [[ ⢀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟  ]],
+--     [[ ⢸⣿⣦⣌⣛⣻⣿⣿⣧⠙⠛⠛⡭⠅⠒⠦⠭⣭⡻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃  ]],
+--     [[ ⠘⣿⣿⣿⣿⣿⣿⣿⣿⡆⠄⠄⠄⠄⠄⠄⠄⠄⠹⠈⢋⣽⣿⣿⣿⣿⣵⣾⠃  ]],
+--     [[  ⠘⣿⣿⣿⣿⣿⣿⣿⣿⠄⣴⣿⣶⣄⠄⣴⣶⠄⢀⣾⣿⣿⣿⣿⣿⣿⠃   ]],
+--     [[   ⠈⠻⣿⣿⣿⣿⣿⣿⡄⢻⣿⣿⣿⠄⣿⣿⡀⣾⣿⣿⣿⣿⣛⠛⠁    ]],
+--     [[     ⠈⠛⢿⣿⣿⣿⠁⠞⢿⣿⣿⡄⢿⣿⡇⣸⣿⣿⠿⠛⠁      ]],
+--     [[        ⠉⠻⣿⣿⣾⣦⡙⠻⣷⣾⣿⠃⠿⠋⠁     ⢀⣠⣴ ]],
+--     [[ ⣿⣿⣿⣶⣶⣮⣥⣒⠲⢮⣝⡿⣿⣿⡆⣿⡿⠃⠄⠄⠄⠄⠄⠄⠄⣠⣴⣿⣿⣿ ]],
+-- }
+
+db.custom_header = {
+[[███████╗██╗  ██╗ █████╗ ███╗   ██╗ ██████╗     ██╗    ██╗███████╗██╗███████╗███████╗███╗   ██╗ ██████╗ ]],
+[[╚══███╔╝██║  ██║██╔══██╗████╗  ██║██╔════╝     ██║    ██║██╔════╝██║██╔════╝██╔════╝████╗  ██║██╔════╝ ]],
+[[  ███╔╝ ███████║███████║██╔██╗ ██║██║  ███╗    ██║ █╗ ██║█████╗  ██║█████╗  █████╗  ██╔██╗ ██║██║  ███╗]],
+[[ ███╔╝  ██╔══██║██╔══██║██║╚██╗██║██║   ██║    ██║███╗██║██╔══╝  ██║██╔══╝  ██╔══╝  ██║╚██╗██║██║   ██║]],
+[[███████╗██║  ██║██║  ██║██║ ╚████║╚██████╔╝    ╚███╔███╔╝███████╗██║██║     ███████╗██║ ╚████║╚██████╔╝]],
+[[╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝      ╚══╝╚══╝ ╚══════╝╚═╝╚═╝     ╚══════╝╚═╝  ╚═══╝ ╚═════╝ ]],
+}
+
+vim.g.dashboard_custom_footer = {'aaaaaaaaa'}
---
+++ lua/user/lsp/settings/yamlls.lua
@@ -0,0 +1,13 @@
+return{
+    settings = {
+        schemaStore = {
+            url = "https://www.schemastore.org/api/json/catalog.json",
+            enable = true,
+        },
+        yaml = {
+            schemas = {
+                ["D:/Dev/mkdocs/schema.json"] = "mkdocs.yml",
+            }
+        }
+    }
+}
